f901715a912922fa929113153180bc5722e1e4ed
R4chC0de <moneymaker4ver@gmail.com>
2024-06-23 22:39:31 +1000

    first draft of 4 indicator algo

--- mooning.py
+++ mooning.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 import numpy as np
 from graphing import Stocks
 
@@ -9,10 +9,190 @@ from graphing import Stocks
 
 nInst = 50
 currentPos = np.zeros(nInst)
-entryPrice = np.zeros(nInst)
-
+entryInfo = np.empty(nInst)
 
 def getMyPosition(prcSoFar):
+    global currentPos, entryInfo
+
+    # bollinger bands variables
+    ma_period = 21
+    # rsi variables
+    rsi_window = 14
+    overbought_rsi = 70
+    oversold_rsi = 30
+    overbought_s_rsi = 80
+    oversold_s_rsi = 20
+    # macd variables
+    slow_ema = 26
+    fast_ema = 12
+    macd_signal_length = 9
+
+    df = Stocks(prcSoFar)
+    df.bbCalc(ma_period)
+    df.rsiCalc(rsi_window)
+    df.stochRSICalc(rsi_window)
+    df.macdCalc(slow_ema, fast_ema, macd_signal_length)
+
+    today_data = df.data.groupby('Stock').tail(1).reset_index(drop=True)
+    today_price = today_data['Price'] # will return as dataframe rather than series
+    #print(today_price)
+    posLimits = np.array([int(x) for x in 10000 / today_price])
+    #print(posLimits[5])
+    #print(today_price)
+    
+    for stock_id, stock_data in today_data.groupby('Stock'):
+        longOrShort_dict = {}
+        price = float(stock_data['Price'])
+        posLimit = int(10000 / price)
+        currPos = currentPos[stock_id]
+
+        # Bollinger Bands
+        ub = float(stock_data['Upper Band'])
+        lb = float(stock_data['Lower Band'])
+
+        if price <= lb:
+            longOrShort_dict['BB'] = 'Long'
+        elif price >= ub:
+            longOrShort_dict['BB'] = 'Short'
+
+        # RSI
+        rsi = float(stock_data[f'RSI {rsi_window}'])
+
+        if rsi < oversold_rsi:
+            longOrShort_dict['RSI'] = 'Long'
+        elif rsi > overbought_rsi:
+            longOrShort_dict['RSI'] = 'Short'
+
+        # Stochastic RSI
+        s_rsi = float(stock_data[f'StochRSI {rsi_window}'])
+
+        if s_rsi < oversold_s_rsi:
+            longOrShort_dict['Stoch RSI'] = 'Long'
+        elif s_rsi > overbought_s_rsi:
+            longOrShort_dict['Stoch RSI'] = 'Short'
+
+        # MACD
+        macd = float(stock_data['MACD'])
+        macd_signal = float(stock_data['MACD Signal'])
+
+        macd_diff = macd - macd_signal
+        if macd_diff >= 0:
+            longOrShort_dict['MACD'] = 'Long'
+        else:
+            longOrShort_dict['MACD'] = 'Short'
+
+        # position size calculation
+        max_confluence = 4
+        long_strat = []
+        short_strat = []
+        for strat, condition in longOrShort_dict.items():
+            if condition == 'Long':
+                long_strat.append(strat)
+            elif condition == 'Short':
+                short_strat.append(strat)
+        
+        long_counter = len(long_strat)
+        short_counter = len(short_strat)
+
+        if currPos == 0: # if not in a position
+            if long_counter >= short_counter and long_counter > 0:
+                position_size = posLimit * (long_counter/max_confluence)
+                currentPos[stock_id] = position_size
+                entryInfo[stock_id] = {'Position': 'Long', 'Strategies': long_strat, 'Entry Price': price}
+            elif short_counter >= long_counter and short_counter > 0:
+                position_size = -posLimit * (short_counter/max_confluence)
+                currentPos[stock_id] = position_size
+                entryInfo[stock_id] = {'Position': 'Short', 'Strategies': short_strat, 'Entry Price': price}
+        else: # if in a position, check if we need to close it
+            entry = entryInfo[stock_id]
+
+            position = entry['Position']
+            strategies = entry['Strategies']
+
+            if position == 'Long':
+                close_position = False
+
+                for strat in strategies:
+                    if strat == 'BB':
+                        if price >= ub:
+                            close_position = True
+                    elif strat == 'RSI':
+                        if rsi >= overbought_rsi:
+                            close_position = True
+                    elif strat == 'Stoch RSI':
+                        if price > overbought_s_rsi:
+                            close_position = True
+                    elif strat == 'MACD':
+                        if macd_diff < 0:
+                            close_position = True
+
+            elif position == 'Short':
+                close_position = False
+
+                for strat in strategies:
+                    if strat == 'BB':
+                        if price <= lb:
+                            close_position = True
+                    elif strat == 'RSI':
+                        if rsi <= oversold_rsi:
+                            close_position = True
+                    elif strat == 'Stoch RSI':
+                        if price <= oversold_s_rsi:
+                            close_position = True
+                    elif strat == 'MACD':
+                        if macd_diff > 0:
+                            close_position = True
+            
+            if close_position:
+                # close the position
+                currentPos[stock_id] = 0
+                entryInfo[stock_id] = None
+
+                # look for new position
+                longOrShort_dict = {}
+                if price <= lb:
+                    longOrShort_dict['BB'] = 'Long'
+                elif price >= ub:
+                    longOrShort_dict['BB'] = 'Short'
+
+                if rsi <= oversold_rsi:
+                    longOrShort_dict['RSI'] = 'Long'
+                elif rsi >= overbought_rsi:
+                    longOrShort_dict['RSI'] = 'Short'
+
+                if s_rsi <= oversold_s_rsi:
+                    longOrShort_dict['Stoch RSI'] = 'Long'
+                elif s_rsi >= overbought_s_rsi:
+                    longOrShort_dict['Stoch RSI'] = 'Short'
+
+                if macd_diff >= 0:
+                    longOrShort_dict['MACD'] = 'Long'
+                else:
+                    longOrShort_dict['MACD'] = 'Short'
+                
+                long_strat = []
+                short_strat = []
+                for strat, condition in longOrShort_dict.items():
+                    if condition == 'Long':
+                        long_strat.append(strat)
+                    elif condition == 'Short':
+                        short_strat.append(strat)
+                
+                long_counter = len(long_strat)
+                short_counter = len(short_strat)
+
+                if long_counter >= short_counter and long_counter > 0:
+                    position_size = posLimit * (long_counter/max_confluence)
+                    currentPos[stock_id] = position_size
+                    entryInfo[stock_id] = {'Position': 'Long', 'Strategies': long_strat, 'Entry Price': price}
+                elif short_counter >= long_counter and short_counter > 0:
+                    position_size = -posLimit * (short_counter/max_confluence)
+                    currentPos[stock_id] = position_size
+                    entryInfo[stock_id] = {'Position': 'Short', 'Strategies': short_strat, 'Entry Price': price}
+
+    return
+
+def bbStrat_getMyPosition(prcSoFar):
     global currentPos, entryPos
     df = Stocks(prcSoFar)
     bb_res = df.bbCalc()
@@ -65,4 +245,22 @@ def _getMyPosition(prcSoFar):
     return currentPos
 
 if __name__ == '__main__':
-    print("hello")
+    nInst = 0
+    nt = 0
+    commRate = 0.0010
+    dlrPosLimit = 10000
+
+    def loadPrices(fn):
+        global nt, nInst
+        df = pd.read_csv(fn, sep='\s+', header=None, index_col=None)
+        (nt, nInst) = df.shape
+        return (df.values).T
+
+
+    pricesFile = "./prices.txt"
+    prcAll = loadPrices(pricesFile)
+
+    (_, nt) = prcAll.shape
+    prcHistSoFar = prcAll[:, :250]
+
+    getMyPosition(prcHistSoFar)
